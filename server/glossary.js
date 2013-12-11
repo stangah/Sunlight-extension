94bd2d493f754c2f2ec521553190b434c0188e8c
stangah <pstanger@gmail.com>
2013-12-10 17:37:19 -0800

    Removed API key for github

--- server/glossary.js
+++ server/glossary.js
@@ -6,7 +6,7 @@ var request = require('request'),
 module.exports.populate = function() {
   var words = [];
 
-  request('https://api.github.com/repos/unitedstates/glossary/contents/definitions/congress?ref=gh-pages&access_token=' + process.env.GITHUB_API, function(err, res, body) {
+  request('https://api.github.com/repos/unitedstates/glossary/contents/definitions/congress?ref=gh-pages', function(err, res, body) {
     if (!err && res.statusCode == 200) {
       body = JSON.parse(body);
       for (var i = 0; i < body.length; i++) {
